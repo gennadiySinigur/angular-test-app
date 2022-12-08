a1794e037171100111ceeeca295cb74f3a020920
gennadiySinigur <gennadiysinigur@gmail.com>
2022-12-08 19:58:19 +0200

    Set routes for login and admin pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './core/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
