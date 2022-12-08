44b8678d513026e6ab58476f2fb089adc6f8b5b7
gennadiySinigur <gennadiysinigur@gmail.com>
2022-12-08 22:08:16 +0200

    Added Login Form and base structure for it

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent
   ]
 })
 export class CoreModule { }
