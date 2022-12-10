14f673db415e13e8a0bb0ae366ea9a71dfbb96b4
gennadiySinigur <gennadiysinigur@gmail.com>
2022-12-10 16:27:40 +0200

    Registered components for user-info

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminComponent } from './admin.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { UserIntroComponent } from './components/user-intro/user-intro.component';
+import { UserTargetsComponent } from './components/user-targets/user-targets.component';
+import { UserStateComponent } from './components/user-state/user-state.component';
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    UserInfoComponent,
+    UserIntroComponent,
+    UserTargetsComponent,
+    UserStateComponent
   ],
   imports: [
     CommonModule
