d0deeb9e1029b3d37895b8b2cb3402e54ac96aa2
gennadiySinigur <gennadiysinigur@gmail.com>
2022-12-10 23:09:16 +0200

    Displayed random count of reactions

--- src/app/admin/components/user-state/user-state-cards/user-state-cards.component.ts
+++ src/app/admin/components/user-state/user-state-cards/user-state-cards.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UtilsService } from '../../../../shared/services/utils.service';
+
 @Component({
   selector: 'app-user-state-cards',
   templateUrl: './user-state-cards.component.html',
   styleUrls: ['./user-state-cards.component.scss']
 })
 export class UserStateCardsComponent implements OnInit {
+  likesCount: number = 0;
+  heartsCount: number = 0;
+  smilesCount: number = 0;
+  MIN: number = 30000;
+  MAX: number = 50000;
 
-  constructor() { }
+  constructor(private utilsService: UtilsService) { }
 
   ngOnInit(): void {
+    this.getRandomReactions(this.MIN, this.MAX);
   }
 
+  getRandomReactions(min: number, max: number): void {
+    this.likesCount = this.utilsService.getRandomNumberInRange(min, max);
+    this.heartsCount = this.utilsService.getRandomNumberInRange(min, max);
+    this.smilesCount = this.utilsService.getRandomNumberInRange(min, max);
+  }
 }
