c921ef1669e900d17b8ee4acdb019fd78e946130
gennadiySinigur <gennadiysinigur@gmail.com>
2022-12-10 14:55:00 +0200

    Got current user data from store

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 import { AuthenticationService } from '../core/services/authentication.service';
+import { User } from '../core/models/User';
 
 @Component({
   selector: 'app-admin',
@@ -9,6 +11,9 @@ import { AuthenticationService } from '../core/services/authentication.service';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  user$: Observable<User> = new Observable<User>();
+  firstName$: Observable<string> = new Observable<string>();
+  lastName$: Observable<string> = new Observable<string>();
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -16,6 +21,13 @@ export class AdminComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.user$ = this.authenticationService.user$;
+    this.firstName$ = this.user$.pipe(
+      map((data) => data.firstName)
+    );
+    this.lastName$ = this.user$.pipe(
+      map((data) => data.lastName)
+    )
   }
 
   logout(): void {
