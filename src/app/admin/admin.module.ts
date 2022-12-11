4df8cffa2b6df8a3d77d36a2c18c2472b78b8c36
gennadiySinigur <gennadiysinigur@gmail.com>
2022-12-11 11:59:56 +0200

    Created request for random user

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AdminComponent } from './admin.component';
 import { UserInfoComponent } from './components/user-info/user-info.component';
@@ -24,7 +25,8 @@ import { SmileIconComponent } from './components/smile-icon/smile-icon.component
     SmileIconComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
   ],
   exports: [
     AdminComponent
--- src/app/admin/services/users-api.service.ts
+++ src/app/admin/services/users-api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+import { RandomUserData } from '../models/randomUserData';
+import { RandomUser } from '../models/randomUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRandomUser(): Observable<RandomUser> {
+    return this.http.get<RandomUserData>('https://randomuser.me/api/').pipe(
+      map((data: RandomUserData): RandomUser => data.results[0])
+    );
+  }
 }
