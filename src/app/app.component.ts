2b5e801b32a332339f0fd97cd3e1d1a8f9eaf3bb
gennadiySinigur <gennadiysinigur@gmail.com>
2022-12-11 23:51:06 +0200

    Hided sidebar in login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-test-app';
+  currentRoute: string = '';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.getCurrentRoute();
+  }
+
+  getCurrentRoute(): void {
+    this.router.events.pipe(
+      filter((event: any): boolean => event instanceof NavigationEnd)
+    ).subscribe((event: { url: string; }) => {
+      this.currentRoute = event.url;
+    })
+  }
+
+  isLoginPage(): boolean {
+    return this.currentRoute === '/login';
+  }
 }
