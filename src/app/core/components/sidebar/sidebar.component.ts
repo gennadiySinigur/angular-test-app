e0af5d88bb6623a663dacfecce242e6b5dbe654b
gennadiySinigur <gennadiysinigur@gmail.com>
2022-12-11 23:06:06 +0200

    Displayed current user's name in sidebar

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+import { User } from '../../models/User';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  user$: Observable<User> = new Observable<User>();
+  firstName$: Observable<string> = new Observable<string>();
+  lastName$: Observable<string> = new Observable<string>();
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.getCurrentUserName();
   }
 
+  getCurrentUserName(): void {
+    this.user$ = this.authenticationService.user$;
+    this.firstName$ = this.user$.pipe(
+      map((data) => data.firstName)
+    );
+    this.lastName$ = this.user$.pipe(
+      map((data) => data.lastName)
+    )
+  }
 }
